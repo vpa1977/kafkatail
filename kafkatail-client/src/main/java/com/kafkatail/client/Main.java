140749909bd27f86c332a1399fdb26f50fb69450
Vladimir <vpa1977@gmail.com>
2022-08-29 19:03:34 +1200

    converting to gradle

--- kafkatail-client/src/main/java/com/kafkatail/client/Main.java
+++ kafkatail-client/src/main/java/com/kafkatail/client/Main.java
@@ -1,8 +1,21 @@
 package com.kafkatail.client;
 
+import com.kafkatail.Tailgrpc.Topic;
+import com.kafkatail.Tailgrpc.TopicCreateRequest;
+import com.kafkatail.TopicsServiceGrpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 public class Main {
 	public static void main(String[] args) {
-		kafkatail.domain.TopicsServiceGrpc.TopicsServiceStub stub = new();
-		
+	    ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080").usePlaintext().build();
+        var blockingStub = TopicsServiceGrpc.newBlockingStub(channel);
+        var createReq = TopicCreateRequest
+        		.newBuilder()
+        		.setTopic(Topic.newBuilder().setName("test").build())
+        		.build();
+        
+		blockingStub.createTopic(createReq);
 	}
 }
