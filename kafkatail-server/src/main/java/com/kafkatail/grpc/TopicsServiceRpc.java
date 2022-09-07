2538e365d3b8316f6d84e359b5285f5246496bc4
Vladimir <vpa1977@gmail.com>
2022-09-07 20:46:19 +1200

    initial rpc implementation

--- kafkatail-server/src/main/java/com/kafkatail/grpc/TopicsServiceRpc.java
+++ kafkatail-server/src/main/java/com/kafkatail/grpc/TopicsServiceRpc.java
@@ -9,45 +9,82 @@ import com.kafkatail.Tailgrpc.TopicUnsubscribeRequest;
 import com.kafkatail.Tailgrpc.Topics;
 import com.kafkatail.Tailgrpc.Void;
 import com.kafkatail.TopicsServiceGrpc.TopicsServiceImplBase;
+import com.kafkatail.service.KafkaAdmin;
+import com.kafkatail.service.KafkaMessageService;
 import io.grpc.stub.StreamObserver;
+import java.util.concurrent.ExecutionException;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @GrpcService
 public class TopicsServiceRpc extends TopicsServiceImplBase {
 
+  @Autowired private KafkaMessageService _messageService;
+  @Autowired private KafkaAdmin _admin;
+
   @Override
   public void getTopics(Void request, StreamObserver<Topics> responseObserver) {
-    // TODO Auto-generated method stub
-    super.getTopics(request, responseObserver);
+    var topicsBuilder = Topics.newBuilder();
+    try {
+      for (var topic : _admin.listTopics())
+        topicsBuilder.addTopic(Topic.newBuilder().setName(topic).build());
+      var message = topicsBuilder.build();
+      responseObserver.onNext(message);
+      responseObserver.onCompleted();
+    } catch (ExecutionException | InterruptedException e) {
+      e.printStackTrace(); // TODO: add logger
+      responseObserver.onError(e);
+    }
   }
 
   @Override
   public void removeTopic(TopicRemoveRequest request, StreamObserver<Topic> responseObserver) {
-    // TODO Auto-generated method stub
-    super.removeTopic(request, responseObserver);
+    var topic = request.getTopic();
+    try {
+      var response = Topic.newBuilder().setName(_admin.removeTopic(topic.getName())).build();
+      responseObserver.onNext(response);
+      responseObserver.onCompleted();
+    } catch (InterruptedException | ExecutionException e) {
+      e.printStackTrace();
+      responseObserver.onError(e);
+    }
   }
 
   @Override
   public void createTopic(TopicCreateRequest request, StreamObserver<Topic> responseObserver) {
-    // TODO Auto-generated method stub
-    super.createTopic(request, responseObserver);
+    var topic = request.getTopic();
+    try {
+      var uuid =
+          _admin.createTopic(
+              topic.getName(), request.getPartitions(), (short) request.getReplicationFactor());
+      var response = Topic.newBuilder().setName(uuid != null ? topic.getName() : null).build();
+      responseObserver.onNext(response);
+      responseObserver.onCompleted();
+    } catch (InterruptedException | ExecutionException e) {
+      e.printStackTrace();
+      responseObserver.onError(e);
+    }
   }
 
   @Override
   public void subscribe(TopicSubscribeRequest request, StreamObserver<Void> responseObserver) {
-    // TODO Auto-generated method stub
-    super.subscribe(request, responseObserver);
+    _messageService.subscribe(request.getTopic().getName());
+    responseObserver.onCompleted();
   }
 
   @Override
   public void unsubscribe(TopicUnsubscribeRequest request, StreamObserver<Void> responseObserver) {
-    // TODO Auto-generated method stub
-    super.unsubscribe(request, responseObserver);
+    _messageService.unsubscribe(request.getTopic().getName());
+    responseObserver.onCompleted();
   }
 
   @Override
   public void subscriptions(Void request, StreamObserver<Topics> responseObserver) {
-    // TODO Auto-generated method stub
-    super.subscriptions(request, responseObserver);
+    var topicsBuilder = Topics.newBuilder();
+    for (var topic : _messageService.subscriptions())
+      topicsBuilder.addTopic(Topic.newBuilder().setName(topic).build());
+    var message = topicsBuilder.build();
+    responseObserver.onNext(message);
+    responseObserver.onCompleted();
   }
 }
